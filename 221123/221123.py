630af40ac0584a4dff13ea0f0a32008da6cb4beb
rotkiW285 <105926196+rotkiW285@users.noreply.github.com>
2022-11-23 08:51:51 +0100

    Update 221123.py

--- 221123/221123.py
+++ 221123/221123.py
@@ -6,6 +6,14 @@ zółw.shape('turtle')
 zółw.color('red')
 zółw.circle(50)
 zółw.left(45)
+zółw.color('green')import turtle
+
+zółw = turtle.Turtle()
+zółw.shape('turtle')
+
+zółw.color('red')
+zółw.circle(50)
+zółw.left(45)
 zółw.color('green')
 zółw.circle(50)
 zółw.left(45)
@@ -30,9 +38,6 @@ zółw.left(45)
 zółw.right(90)
 zółw.color('black')
 zółw.forward(200)
-
-zółw.penup()
-zółw.goto(-200,0)
 
 
 turtle.exitonclick()
