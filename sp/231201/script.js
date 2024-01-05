9ae1709cd30670a9dc641d075820013f58deba1a
Wiktor Stawicki <105926196+rotkiW285@users.noreply.github.com>
2024-01-05 12:49:17 +0100

    changing the way the interpretation of the result is displayed
    in 231201

--- sp/231201/script.js
+++ sp/231201/script.js
@@ -10,16 +10,29 @@ function bmi(){
         d=b/100
         c=a/(d*d)
         document.getElementById("bmiwynik").innerHTML = c.toFixed(2);
-        if(c<=18.5){
-            document.getElementById("waga").innerHTML = "<span style='color: red;'>BMI za mało</span>";
+        if(c<=16){
+            document.getElementById("waga").innerHTML = "<span style='color: red;'>Wygłodzenie</span>";
+        }
+        else if(c>16 && c<=16.9){
+            document.getElementById("waga").innerHTML = "<span style='color: orange;'>Wychudzenie</span>";
+        }
+        else if(c>17 && c<=18.4){
+            document.getElementById("waga").innerHTML = "<span style='color: yellow;'>Niedowaga</span>";
+        }
+        else if(c>=18.5 && c<=24.9){
+            document.getElementById("waga").innerHTML = "<span style='color: green;'>Waga prawidłowa</span>";
+        }
+        else if(c>=25 && c<=29.9){
+            document.getElementById("waga").innerHTML = "<span style='color: yellow;'>Nadwaga</span>";
+        }
+        else if(c>=30 && c<=34.9){
+            document.getElementById("waga").innerHTML = "<span style='color: orange;'>Otyłość I stopnia</span>";
+        }
+        else if(c>=35 && c<=39.9){
+            document.getElementById("waga").innerHTML = "<span style='color: red;'>Otyłość II stopnia</span>";
         }
         else{
-            if(c>=25){
-                document.getElementById("waga").innerHTML = "<span style='color: red;'>BMI za dużo</span>";
-            }
-            else{
-                document.getElementById("waga").innerHTML = "<span style='color: green;'>BMI w normie!</span>";
-            }
+            document.getElementById("waga").innerHTML = "<span style='color: black;'>Otyłość skrajna</span>";
         }
-        }
+    }
 }
