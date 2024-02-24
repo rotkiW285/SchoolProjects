ca787b3d1ac89267300b691a13ef44e4bc7dedaf
Wiktor Stawicki <105926196+rotkiW285@users.noreply.github.com>
2024-02-24 23:10:23 +0100

    complete redesign of navbar

---
+++ navbar.js
@@ -0,0 +1,51 @@
+class Navbar {
+    constructor() {
+        this.links = [
+            { title: '𝕊CHOOL PROJECTS', url: 'index.html'},
+            { title: 'STARSZAKI', url: 'starszaki.html'},
+            { title: 'INNE STRONY', url: 'innestrony.html'},
+            { title: 'SBD & PBD', url: 'php.html'},
+            { title: 'PYTHON', url: 'python.html'},
+            { title: 'INNE', url: 'inne.html'},
+            { title: 'EE-09', url: 'https://jankupczyk.github.io/EE-09/' },
+            { title: '© rotkiW285', url: 'info.html'}
+        ];
+    }
+
+    render() {
+        const navbarElement = document.createElement('nav');
+        const brElement = document.createElement('br');
+        navbarElement.classList.add('nav');
+
+        const isSmallDevice = window.innerWidth <= 800;
+
+        if (isSmallDevice) {
+            const ulElement = document.createElement('ul');
+            ulElement.classList.add('nav-list');
+
+            this.links.forEach((link, index) => {
+                const liElement = document.createElement('li'); 
+                const aElement = document.createElement('a');
+                aElement.href = link.url;
+                aElement.textContent = link.title;
+                aElement.classList.add(link.class); 
+                liElement.appendChild(aElement); 
+                ulElement.appendChild(liElement); 
+            });
+
+            navbarElement.appendChild(ulElement);
+        } else {
+            this.links.forEach((link, index) => {
+                const aElement = document.createElement('a');
+                aElement.href = link.url;
+                aElement.textContent = link.title;
+                aElement.classList.add(link.class);
+                navbarElement.appendChild(aElement);
+            });
+        }
+        document.body.appendChild(navbarElement);
+    }
+}
+
+const navbar = new Navbar();
+navbar.render();
