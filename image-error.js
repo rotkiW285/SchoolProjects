0c93b39a7ad9e50b0aefae91ee84f3ce56fc220d
rotkiW285 <105926196+rotkiW285@users.noreply.github.com>
2024-09-03 23:42:54 +0200

    image error script

---
+++ image-error.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const galleryImages = document.querySelectorAll('.gimg img');
+
+    galleryImages.forEach(function (img) {
+        img.onerror = function () {
+            this.onerror = null;
+            this.src = 'error.webp';
+        };
+    });
+});
